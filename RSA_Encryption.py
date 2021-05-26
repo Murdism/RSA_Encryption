c0b35326bbe97d88213a642a849e481872d1e830
Murdism <63874457+Murdism@users.noreply.github.com>
2021-05-27 03:10:26 +0400

    Final Version with graph update and improved modulo inverse

--- RSA_Encryption.py
+++ RSA_Encryption.py
@@ -1,5 +1,8 @@
 import random
 import sys
+import time
+# importing the required module
+import matplotlib.pyplot as plt
 
 
 class RSACryptoSystem:
@@ -19,8 +22,8 @@ class RSACryptoSystem:
         return r
 
     def generateRandomInt(self, n):
-        """returns a random integer a such that 2 <= a <= n-2 """
-        return random.randint(1, n - 2)
+        """returns a random integer a such that 1 <= a <= n-1 """
+        return random.randint(1, (n - 1))
 
     def gcd(self, a, b):
         """returns greater common divisor of a and b."""
@@ -37,19 +40,21 @@ class RSACryptoSystem:
     def generatePrimes(self):
         """generate p, and q prime number"""
         p, q = 0, 0
-
+        num_random_tested=0
         for i in range(1, 3):
             x = self.generate_odd_int()
+            num_random_tested+=1
             value = self.miller_rabin_primeTest(x)
             while not (value == "prime" and x != p):
                 x = self.generate_odd_int()
+                num_random_tested+=1
                 value = self.miller_rabin_primeTest(x)
             if i == 1:
                 p = x
             else:
                 q = x
 
-        return p, q
+        return p, q,num_random_tested
 
     def miller_rabin_primeTest(self, n):
         """Miller - Rabin Primality testing Algs.
@@ -88,9 +93,8 @@ class RSACryptoSystem:
     def moduloInverse(self, e, rP):
         """ return integer d such that d = m^-1 mod n =  gcd(m*d, rP) == 1 """
         gcd, x, y = self.etx_gcd(e, rP)
-        if gcd == 1:
-            return x % rP
-        return False  # in case no inverse
+        return x % rP
+      
 
     def generateKeyPairs(self, p, q):
         """get a small e which is a relative prime to rp = (p-1)*(q-1).
@@ -98,7 +102,7 @@ class RSACryptoSystem:
         """
         rP = (p - 1) * (q - 1)  # get the relative co-prime, Q(n)
         possiblePublicKeys = []  # for big nBits,  huge memory
-        for e in range(2, rP):  # O(n)
+        for e in range(3, rP):  # O(n)
             if self.gcd(e, rP) == 1:
                 possiblePublicKeys.append(e)
                 if len(possiblePublicKeys) == 100:
@@ -132,6 +136,75 @@ class RSACryptoSystem:
         """returns if input N is an integer"""
         bit_size = int(input("Enter the bit size:"))  # N bits integers or N = 512 bits
         self.nBits = bit_size
+    def set_bitSize(self,bsize):
+        """returns if input N is an integer"""
+        self.nBits = bsize
+
+
+def graph_nbit_time(g): # if g=1 N_bits vs randomly tested numbers for primality else if g=0 N_bits vs Time 
+        rsa_graph = RSACryptoSystem()  
+        bit_size_tested=[32,64,128,256,512,768,1024,1200,1512,2024]
+        time_taken_only=[]
+        time_nbit=[]
+        random_primality_nbit=[] # number of bits and number of randomly tested values for primality
+        keys_arranged=[]
+        num_of_random=[] #number of random numbers tested_
+        for i in bit_size_tested:
+            start = time.time()
+            temp_key_list=[]
+            time_nbit_temp=[]
+            random_primality_nbit_temp=[]
+            # get bit_size input
+            rsa_graph.set_bitSize(i)
+            
+            p, q, random_num_tested = rsa_graph.generatePrimes()
+            #print(f"p {p},q = {q}")
+            num_of_random.append(random_num_tested) 
+
+            privateKey, publicKey = rsa_graph.generateKeyPairs(p, q)
+            temp_key_list.append(privateKey)
+            temp_key_list.append(publicKey)
+            print(f"////////////////////////////////// ---Bit_size {i}----///////////////////")
+            print(f"private key : {privateKey}, \n public key : {publicKey}")
+            
+            end = time.time()
+            elapsed= end-start
+            temp_key_list.append(elapsed)
+            # key arranged has public,private and time elapsed
+            keys_arranged.append(temp_key_list)
+            # time_nbit_temp a list of one iteration that contains bit_size and time elapsed
+            time_nbit_temp.append(i)
+            time_nbit_temp.append(elapsed)
+            # time_nbit contains bit size and time elapsed of all iterations
+            time_nbit.append(time_nbit_temp)
+            # Saves only time taken at each bit entry
+            time_taken_only.append(elapsed)
+
+            #N bits and number of Primality tested integers
+            random_primality_nbit_temp.append(i) 
+            random_primality_nbit_temp.append(random_num_tested) 
+            random_primality_nbit.append(random_primality_nbit_temp)
+           
+        # Plot N_bits vs time
+        if(g==0):
+            print("N_bits vs Time to find prime numbers",time_nbit)
+            #plot bit_size vs time taken
+            plt.plot(bit_size_tested,time_taken_only)
+            plt.xlabel('Bit_size')
+            plt.ylabel('Time(secs)')
+            plt.title('Bit_size vs Time')
+            plt.show()
+        
+        else:
+            # Plot N_bits Vs Tested random int number (how many times did it chnage selected random value to find p and q)
+            print("N_bits vs randomly tested numbers for primality",random_primality_nbit)
+            #plot bit_size vs time taken
+            plt.plot(bit_size_tested,num_of_random)
+            plt.xlabel('Bit_size')
+            plt.ylabel('NO. of Primality Tested Integers')
+            plt.title('Bit_size vs No. Of Primality Tested Integers')
+            plt.show()
+
 
 
 def main():
@@ -139,10 +212,10 @@ def main():
     rsa = RSACryptoSystem()  # create RSA object
 
     choice = input(
-        "Enter 'e' for encryption,'d' for decryption or 'g' to generate keys:"
+        "Enter 'e' for encryption,'d' for decryption or 'g' to generate keys , graph-to generate graph:"
     )
     if choice == "e":
-        message, e, n = input("inter message, e, and n:").split(" ")
+        message, e, n = input("Enter message, e, and n:").split(" ")
         message, e, n = int(message), int(e), int(n)
 
         encyptedMessage = rsa.encryptMessage(message, e, n)
@@ -158,11 +231,19 @@ def main():
         # get bit_size input
         rsa.get_bitSize()
 
-        p, q = rsa.generatePrimes()
+        p, q, random_num_tested= rsa.generatePrimes()
         print(f"p {p},q = {q}")
 
         privateKey, publicKey = rsa.generateKeyPairs(p, q)
         print(f"private key : {privateKey}, public key : {publicKey}")
+
+    elif choice == "graph": # generates random keys of differenent size to compare time
+
+        #if g-> input=1 N_bits vs randomly tested values for primality else if g=0 N_bits vs Time 
+
+        graph_nbit_time(0)
+
+
     elif choice == "x":
         sys.exit()
     else:
